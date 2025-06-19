551b54ec45a14ae6c360e2735eb9cc5ccd9582eb
FLADavidentor <daviderossanino@gmail.com>
2025-06-19 15:37:23 -0600

    Limpieza leve de dtos deprecados

--- src/main/java/com/uam/agendave/dto/Registro/AsistenciaDTO.java
+++ src/main/java/com/uam/agendave/dto/Registro/AsistenciaDTO.java
@@ -1,6 +1,8 @@
 package com.uam.agendave.dto.Registro;
 
 import com.uam.agendave.model.EstadoAsistencia;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,7 +12,10 @@ import java.util.UUID;
 @Setter
 public class AsistenciaDTO {
 
+    @NotBlank(message = "El CIF del estudiante es obligatorio")
     private String cif;
+    @NotNull(message = "El id de Actividad es obligatorio")
     private UUID idActividad;
+    @NotNull(message = "Debe de incluirse el Estado de Asistencia")
     private EstadoAsistencia estadoAsistencia;
 }
