a57dd2767b72a9331a0244201bc8cba1aa9cd290
FLADavidentor <daviderossanino@gmail.com>
2025-07-12 10:46:07 -0600

    Imagenes promocionales agregadas

--- src/main/java/com/uam/agendave/controller/PromoImagesController.java
+++ src/main/java/com/uam/agendave/controller/PromoImagesController.java
@@ -1,11 +1,13 @@
 package com.uam.agendave.controller;
 
 import com.uam.agendave.dto.Imagen.PromoImagesRequest;
+import com.uam.agendave.dto.Imagen.PromoImagesResponse;
 import com.uam.agendave.service.imagen.ImagenPromoService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -20,22 +22,24 @@ public class PromoImagesController {
     @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/create")
     public ResponseEntity<?> create(@RequestBody PromoImagesRequest promoImagesRequest) {
-        try {
-            return imagenPromoService.CrearImagenPromo(promoImagesRequest);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-            return ResponseEntity.internalServerError().body(Map.of(
-                    "success", false,
-                    "message", "Ocurrio un error al crear la imagen",
-                    "error", ex.getMessage()
-            ));
-        }
+
+        return imagenPromoService.CrearImagenPromo(promoImagesRequest);
+
     }
 
     @PreAuthorize("hasAnyRole('ADMIN','ESTUDIANTE')")
     @GetMapping("/get_promo")
     public ResponseEntity<?> getPromo() {
+
         return imagenPromoService.ObtenerImagenesPromo();
+
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @PostMapping("/delete")
+    public ResponseEntity<?> delete(@RequestBody List<PromoImagesResponse> promoImagesResponses) {
+
+        return imagenPromoService.EliminarImagenPromo(promoImagesResponses);
 
     }
 }
