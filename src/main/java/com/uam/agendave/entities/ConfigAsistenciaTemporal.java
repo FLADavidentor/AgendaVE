7ed0a7e9ff6a9a6ff2538c3f506e77ad691227a6
FLADavidentor <129012786+FLADavidentor@users.noreply.github.com>
2025-07-13 21:56:11 -0600

    Update ConfigAsistenciaTemporal.java
    cambio para que sirva

--- src/main/java/com/uam/agendave/entities/ConfigAsistenciaTemporal.java
+++ src/main/java/com/uam/agendave/entities/ConfigAsistenciaTemporal.java
@@ -7,11 +7,18 @@ import lombok.Setter;
 import java.time.LocalDateTime;
 
 @Data
+@Getter
+@Setter
 public class ConfigAsistenciaTemporal {
     private Double radioMetros;
     private LocalDateTime tiempoLimite;
+    private Double lat;
+    private Double lng;
+
 
     public ConfigAsistenciaTemporal(Double radioMetros, LocalDateTime tiempoLimite) {
+        this.lat = lat;
+        this.lng = lng;
         this.radioMetros = radioMetros;
         this.tiempoLimite = tiempoLimite;
     }
